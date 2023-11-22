fee1a4e8477b22d0805256d86c51d28197f11d9c
Efa-Isnawati <efaisnawati1911@gmail.com>
2023-11-22 21:57:06 +0700

    update home

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,20 @@ function myFunction() {
     navbar.classList.remove("fixed");
   }
 }
+document.getElementById("btnSwitch").addEventListener("click", () => {
+  const currentTheme = document.documentElement.getAttribute("data-bs-theme");
+
+  if (currentTheme == "dark") {
+    document.documentElement.setAttribute("data-bs-theme", "light");
+    document
+      .getElementById("navbar")
+      .classList.remove("navbar-dark", "bg-dark");
+    document.getElementById("navbar").classList.add("navbar-light", "bg-light");
+  } else {
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+    document
+      .getElementById("navbar")
+      .classList.remove("navbar-light", "bg-light");
+    document.getElementById("navbar").classList.add("navbar-dark", "bg-dark");
+  }
+});
